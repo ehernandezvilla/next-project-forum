e612a642cc112b6a1114ba92de26bb0fd4e126d9
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-05-18 22:31:45 -0400

    using usePathname()

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -1,25 +1,31 @@
 'use client'
+import path from 'path'
 import Button from '../button/Button'
 import styles from './Nav.module.css'
-export default function Nav() {
-  const pathname = "REPLACE ME"
+import { usePathname } from 'next/navigation'
+
+
+
+export default function Nav() {  
+const pathname = usePathname()
+// console.log(pathname)
 
   return (
     <nav>
       <ul className={styles.navUl}>
-        <li>
-          <Button label="Home" href="/" />
+        <li className={pathname =="/" ? styles.active : ""}>
+          <Button label="Home" href="/"/>
         </li>
-        <li>
+        <li className={pathname =="/about" ? styles.active : ""}>
           <Button label="About" href="/about" />
         </li>
-        <li>
+        <li className={pathname =="/topics" ? styles.active : ""}>
           <Button label="Topics" href="/topics" />
         </li>
-        <li>
+        <li className={pathname =="/questions" ? styles.active : ""}>
           <Button label="All Questions" href="/questions" />
         </li>
-        <li>
+        <li className={pathname =="/users" ? styles.active : ""}>
           <Button label="All Users" href="/users" />
         </li>
       </ul>
