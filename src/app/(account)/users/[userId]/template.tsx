07db556807fdd6608a61c31684315c7f442c2a73
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-05-18 22:36:26 -0400

    adding template.tsx

---
+++ src/app/(account)/users/[userId]/template.tsx
@@ -0,0 +1,15 @@
+
+
+export default function UserTemplate({children} : {children: React.ReactNode}) {
+
+const requestTime = new Date()
+
+return (
+  <div>
+    {children}
+    <footer>Last Checked: {requestTime.toLocaleTimeString()}</footer>
+  </div>
+)
+
+}
+
